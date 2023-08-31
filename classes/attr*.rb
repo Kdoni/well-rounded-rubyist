fd76c033a2ac61a21f1648eed52235cbe585fdc7
kdoni <chrisvdoni@gmail.com>
2023-08-31 19:04:45 +0300

    Some tricky concepts of class methods.

--- classes/attr*.rb
+++ classes/attr*.rb
@@ -6,4 +6,21 @@ class Ticket
     @venue = venue
     @date = date
   end
+
+  def self.most_expensive(*tickets)
+    tickets.max_by(&:price)
+  end
 end
+
+ticket1 = Ticket.new('Artaki', 'Twra')
+ticket2 = Ticket.new('Aggouristan', 'Pote')
+ticket3 = Ticket.new('Kastella', 'Aurio')
+
+ticket1.price = 10
+puts ticket1.price
+ticket2.price = (11)
+puts ticket2.price
+ticket3.price = 15
+expensive = Ticket.most_expensive(ticket1, ticket2, ticket3)
+puts expensive.venue
+puts expensive.class
